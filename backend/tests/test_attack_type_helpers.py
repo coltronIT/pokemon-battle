9ae63a168e3d5e55913bc373e16c52ac279a5717
colton.duncan <colton.duncan@lis.state.oh.us>
2024-02-13 07:50:00 -0500

    added test

---
+++ backend/tests/test_attack_type_helpers.py
@@ -0,0 +1,34 @@
+import pytest
+
+from backend.helpers.attack_type_helpers import (
+    handle_grass_attack_type,
+    handle_water_attack_type,
+    handle_fire_attack_type,
+    handle_normal_attack_type,
+)
+
+@pytest.mark.parametrize(
+    'pokemon_type, expected_outcome',
+    [
+        ('grass', 'Not Very Effective'),
+        ('water', 'Super Effective'),
+        ('fire', 'Not Very Effective'),
+        ('normal', 'Effective')
+    ]
+)
+def test_grass_attack_type(pokemon_type, expected_outcome):
+    assert handle_grass_attack_type(pokemon_type) == expected_outcome
+
+
+@pytest.mark.parametrize(
+    'pokemon_type, expected_outcome',
+    [
+        ('grass', 'Not Very Effective'),
+        ('water', 'Super Effective'),
+        ('fire', 'Not Very Effective'),
+        ('normal', 'Effective')
+    ]
+)
+def test_water_attack_type(pokemon_type, expected_outcome):
+    assert handle_grass_attack_type(pokemon_type) == expected_outcome
+
