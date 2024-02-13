9db1237da0e9638d084f307bc1aa9a5261e14e06
colton.duncan <colton.duncan@lis.state.oh.us>
2024-02-13 07:49:46 -0500

    more refactor

--- backend/helpers/attack_type_helpers.py
+++ backend/helpers/attack_type_helpers.py
@@ -2,29 +2,20 @@ from backend.enums.EffectivenessEnum import Effectiveness
 from backend.enums.TypeEnum import Type
 
 
-def handle_attack_type_from_mapper(
-    pokemon_type: Type,
-    type_effectiveness_mapper: dict[Type, Effectiveness]
-) -> Effectiveness:
-    for mapper_pokemon_type, effectiveness in type_effectiveness_mapper.items():
-        if pokemon_type == mapper_pokemon_type:
-            return effectiveness
-
-
 def handle_grass_attack_type(pokemon_type: Type) -> Effectiveness:
-    return handle_attack_type_from_mapper(pokemon_type, grass_type_effectiveness_mapper)
+    return grass_type_effectiveness_mapper[pokemon_type]
 
 
 def handle_water_attack_type(pokemon_type: Type) -> Effectiveness:
-    return handle_attack_type_from_mapper(pokemon_type, water_type_effectiveness_mapper)
+    return water_type_effectiveness_mapper[pokemon_type]
 
 
 def handle_fire_attack_type(pokemon_type: Type) -> Effectiveness:
-    return handle_attack_type_from_mapper(pokemon_type, fire_type_effectiveness_mapper)
+    return fire_type_effectiveness_mapper[pokemon_type]
 
 
 def handle_normal_attack_type(pokemon_type: Type) -> Effectiveness:
-    return handle_attack_type_from_mapper(pokemon_type, normal_type_effectiveness_mapper)
+    return normal_type_effectiveness_mapper[pokemon_type]
 
 
 def determine_effectiveness(attack_type: Type, pokemon_type: Type) -> None | Effectiveness:
@@ -35,24 +26,25 @@ def determine_effectiveness(attack_type: Type, pokemon_type: Type) -> None | Eff
         if attack_type == mapper_attack_type:
             return attack_handler(pokemon_type)
 
+
 grass_type_effectiveness_mapper = {
     Type.FIRE: Effectiveness.NOT_VERY_EFFECTIVE,
     Type.WATER: Effectiveness.SUPER_EFFECTIVE,
-    Type.GRASS: Effectiveness.EFFECTIVE,
+    Type.GRASS: Effectiveness.NOT_VERY_EFFECTIVE,
     Type.NORMAL: Effectiveness.EFFECTIVE
 }
 
 fire_type_effectiveness_mapper = {
     Type.WATER: Effectiveness.NOT_VERY_EFFECTIVE,
     Type.GRASS: Effectiveness.SUPER_EFFECTIVE,
-    Type.FIRE: Effectiveness.EFFECTIVE,
+    Type.FIRE: Effectiveness.NOT_VERY_EFFECTIVE,
     Type.NORMAL: Effectiveness.EFFECTIVE
 }
 
 water_type_effectiveness_mapper = {
     Type.GRASS: Effectiveness.NOT_VERY_EFFECTIVE,
     Type.FIRE: Effectiveness.SUPER_EFFECTIVE,
-    Type.WATER: Effectiveness.EFFECTIVE,
+    Type.WATER: Effectiveness.NOT_VERY_EFFECTIVE,
     Type.NORMAL: Effectiveness.EFFECTIVE
 }
 
